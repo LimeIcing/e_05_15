69d6f57e70c2a86da0cdc3c387cd3af33297812e
Lime Icing <emil577b@stud.kea.dk>
2018-05-23 23:20:45 +0200

    corrected fatal error

--- 15.sql
+++ 15.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(guestName) FROM Guest
+SELECT COUNT(DISTINCT guestName) FROM Guest
 JOIN Booking ON Guest.guestNo = Booking.guestNo
 WHERE Booking.dateFrom LIKE '%-08-%' OR Booking.dateTo LIKE '%-08-%'
--- db init.sql
+++ db init.sql
@@ -29,26 +29,38 @@ CREATE TABLE Booking (
     guestNo INT,
     dateFrom DATE,
     dateTo DATE,
+    roomNo INT,
     PRIMARY KEY(hotelNo, guestNo, dateFrom),
     FOREIGN KEY(hotelNo) REFERENCES Hotel(hotelNo),
-    FOREIGN KEY(guestNo) REFERENCES Guest(guestNo)
+    FOREIGN KEY(guestNo) REFERENCES Guest(guestNo),
+    FOREIGN KEY(roomNo) REFERENCES Room(roomNo)
 );
 
 INSERT INTO Hotel(hotelName, city)
 VALUES 	('Grand Hotel', 'Copenhagen'),
-		('Welby Studios', 'London');
+		('Welby Studios', 'London'),
+        ('Grosvenor Hotel', 'London');
 
 INSERT INTO Guest(guestName, guestAddress)
 VALUES	('The Collector', 'Knowhere'),
 		('Stephen Strange', 'The Sanctuary'),
-        ('Groot', 'I am Groot');
-
-INSERT INTO Booking
-VALUES	(2, 1, '2018-04-25', '2018-05-03'),
-		(2, 2, '2018-04-01', '2018-04-25'),
-        (1, 3, '2017-08-05', '2018-08-12');
+        ('Groot', 'I am Groot'),
+        ('Tony Stark', 'Stark Tower, New York');
 
 INSERT INTO Room
 VALUES	(1408, 2, 'Single', 0),
 		(232, 2, 'Double', 22),
-		(110, 2, 'Family', 45)
+		(110, 2, 'Family', 45),
+        (422, 2, 'Family', 149),
+        (232, 3, 'Single', 54),
+        (362, 2, 'Double', 105),
+        (430, 2, 'Double', 148),
+        (786, 1, 'Family', 30),
+        (218, 2, 'Single', 104),
+        (900, 3, 'Double', 117);
+
+INSERT INTO Booking
+VALUES	(2, 1, '2018-04-25', '2018-05-03', 110),
+		(2, 2, '2018-04-01', '2018-04-25', 422),
+        (1, 3, '2018-08-05', '2018-08-12', 786),
+        (3, 4, '2018-05-19', '2018-05-27', 900);
